3b18644f316a897658929b5a602e71a5b6a1e3dd
Marek Zborowski <marek7654@gmail.com>
2023-09-24 23:55:29 +0200

    Hero fixes

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -6,15 +6,14 @@ const Hero = () => {
   return (
     <section className={classes.hero}>
       <div className={`${classes.container} container`}>
-        <HeroImage />
-
+        <HeroImage/>
         <div className={classes.contentWrapper}>
           <h1 className={classes.header}>Discover and Find Your Own Fashion!</h1>
           <p className={classes.description}>
             Explore our curated collection of stylish clothing and accessories
             tailored to your unique taste.
           </p>
-          <Button herf="#" title="EXPLORE NOW" styleClass="accent"/>
+          <Button to="shop" title="EXPLORE NOW" styleClass="accent"/>
         </div>
       </div>
     </section>
