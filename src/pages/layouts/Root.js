12765975bb909cc440230cb37f5d283a93802689
Marek Zborowski <marek7654@gmail.com>
2023-09-24 14:12:17 +0200

    Layout styles

--- src/pages/layouts/Root.js
+++ src/pages/layouts/Root.js
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
+
 import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import classes from "./Root.module.css";
 
 const RootLayout = () => {
   return <>
     <Header/>
-    <main>
+    <main className={classes.main} style={{minHeight: `${window.innerHeight - 102}px`}}>
       <Outlet/>
     </main>
+    <Footer/>
   </>
 }
 
