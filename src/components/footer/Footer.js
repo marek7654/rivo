e5a8f960a1e067dbb0c6db59423407f9cc7309ae
Marek Zborowski <marek7654@gmail.com>
2023-09-24 14:11:01 +0200

    Add footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import classes from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <p className={classes.footerText}>© 2023 Rivo</p>
+    </footer>
+  );
+};
+
+export default Footer;
