2d16e488bb1cc6686232d76a9eac8c6b6ae76e39
Marek Zborowski <marek7654@gmail.com>
2023-09-25 05:47:53 +0200

    Mene - add state to hamburger

--- src/components/header/Hamburger.js
+++ src/components/header/Hamburger.js
@@ -1,7 +1,22 @@
 import classes from './Hamburger.module.css';
 
-const Hamburger = () => {
-  return <input className={classes.hamburger} type="checkbox" role="button" aria-label="Display the menu" />;
-}
+const Hamburger = ({ buttonHandler, isOpenState }) => {
+
+  const clickHandler = (e) => {
+    buttonHandler(!isOpenState);
+  };
+
+  return (
+    <input
+      className={classes.hamburger}
+      type='checkbox'
+      role='button'
+      aria-label='Display the menu'
+      value={isOpenState}
+      checked={isOpenState}
+      onChange={clickHandler}
+    />
+  );
+};
 
 export default Hamburger;
--- src/components/header/MainNavigation.js
+++ src/components/header/MainNavigation.js
@@ -1,58 +1,73 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 import Hamburger from './Hamburger';
 
 const MainNavigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const hamburgerHandler = (status) => {
+    setIsOpen(status);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
-    <Hamburger/>
-    
-    <nav className={classes.navigation}>
-      <ul className={classes.list}>
-        <li>
-          <NavLink
-            to=''
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-            end
-          >
-            HOME
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='tmp'
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-          >
-            SHOP
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='tmp'
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-          >
-            FEATURES
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='tmp'
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-          >
-            CONTACT
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+      <Hamburger isOpenState={isOpen} buttonHandler={hamburgerHandler} />
+
+      <nav className={classes.navigation}>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to=''
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+              onClick={closeMenu}
+            >
+              HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='tmp'
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              onClick={closeMenu}
+            >
+              SHOP
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='tmp'
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              onClick={closeMenu}
+            >
+              FEATURES
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='tmp'
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              onClick={closeMenu}
+            >
+              CONTACT
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
